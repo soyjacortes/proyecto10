3baf409c118ca20753bdb2efb958064ba0a575e8
soyjacortes <145300913+soyjacortes@users.noreply.github.com>
2023-11-13 16:51:30 -0500

    Add files via upload

---
+++ proyecto10/script.js
@@ -0,0 +1,12 @@
+const navegacion = document.querySelector(".menuPrincipal");
+const abrir = document.querySelector(".abrirMenu");
+const cerrar = document.querySelector(".cerrarMenu");
+
+
+abrir.addEventListener("click", ()=>{
+    navegacion.classList.add("visible");
+})
+
+cerrar.addEventListener("click", ()=>{
+    navegacion.classList.remove("visible");
+})
